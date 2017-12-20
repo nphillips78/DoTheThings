04e0857847ed1cec88c817ac93a446893383cb41
Nicole Phillips <nphillips78@gmail.com>
2017-12-19 23:12:16 -0500

    button

--- taskapp/src/App.js
+++ taskapp/src/App.js
@@ -1,14 +1,32 @@
-/* import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Joke extends Component {
+  constructor() {
+    super();
+    this.state=
+  }
+}
+
+/* class App extends Component {
   render() {
     return (
       <div>
-        
+        <button className="button" onClick="Happy">
+        <i class="fas fa-smile"></i>
+        </button>
+        </div><div>
+          <button className="button" onClick="Sad">
+          <i class="fas fa-frown"></i>
+          </button>
+          </div> */
+
+
+
+
     );
   }
 } 
 
-export default App; */
+export default App; 
